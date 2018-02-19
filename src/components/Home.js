240ef1261fb4fb98062b5baacb6cdf4e0da18add
pto3 <parker@ptocreativeworks.com>
2018-02-18 20:42:46 -0500

    Indentation/organization

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,18 +11,18 @@ import '../styles/components/home.css';
 
 
 export default class Home extends React.Component {
-    render() {
-        return (
-            <div className="homeContainer">
-                <Navbar />
-                <About />
-                <h2 id="projects" className="contentHeader">Projects</h2>
-                <Projects projects={this.props.projects} />
-                <div className="background"></div>
-                <h2 id="contact" className="contentHeader">Contact me!</h2>
-                <ContactLinks />
-                <Footer />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="homeContainer">
+        <Navbar/>
+        <About/>
+        <h2 id="projects" className="contentHeader">Projects</h2>
+        <Projects projects={this.props.projects}/>
+        <div className="background"></div>
+        <h2 id="contact" className="contentHeader">Contact me!</h2>
+        <ContactLinks/>
+        <Footer/>
+      </div>
+    );
+  }
 }
