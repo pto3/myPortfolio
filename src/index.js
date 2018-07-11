90ae2731c74530b8da0d0810df5faa68e72a8303
Parker O'Brien <obriepar@mail.gvsu.edu>
2018-07-11 12:04:34 -0400

    Moving project data property to project componet.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Home from './components/Home.js'
-import projects from './data/myProjects'
+
 
 import './styles/index.css';
 
 ReactDOM.render(
-    <Home projects={projects} />,
+    <Home/>,
     document.getElementById('root')
 );
