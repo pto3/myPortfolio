44682cdc2eaf1badb495560e22b28eb3d7edc9e0
Parker O'Brien <obriepar@mail.gvsu.edu>
2018-10-10 16:35:52 -0400

    updated mailto link

--- src/components/ContactLinks.js
+++ src/components/ContactLinks.js
@@ -6,7 +6,7 @@ class ContactLinks extends React.Component {
   render() {
     return (
       <div className="contactContainer">
-        <span><a href="mailto:j.parkerobrien3@gmail.com">Email</a></span>
+        <span><a href="mailto:parkerobrien3@gmail.com">Email</a></span>
         <span><a href="https://www.linkedin.com/in/parker-o-brien-8a4605108/">LinkedIn</a></span>
         <span><a href="https://github.com/pto3">github</a></span>
       </div>
